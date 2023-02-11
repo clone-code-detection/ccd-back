277a1201b65ded9244dab09ef3ad451997fdad31
hanh4 <hanh4@vng.com.vn>
2023-02-12 00:02:55 +0700

    Add vertx dependency TODO: Migrate App Test to junit 5 TODO: Implement config util reading from json files TODO: Implement ANTLR4 parser issue: https://clone-code-detection.atlassian.net/browse/CCD-13

--- src/main/java/github/clone_code_detection/App.java
+++ src/main/java/github/clone_code_detection/App.java
@@ -1,13 +1,34 @@
 package github.clone_code_detection;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import github.clone_code_detection.server.HttpServerVerticle;
+import io.vertx.core.Vertx;
+import io.vertx.core.json.jackson.DatabindCodec;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    static private final Logger logger = LoggerFactory.getLogger(App.class);
+
+    public static void main(String[] args) {
+        DatabindCodec.mapper()
+                .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES , false);
+        Injector injector = Guice.createInjector();
+
+        Vertx vertx = injector.getInstance(Vertx.class);
+        HttpServerVerticle httpServerVerticle = injector.getInstance(HttpServerVerticle.class);
+        //TODO: Deploy on all core to maximize performance
+        vertx.deployVerticle(httpServerVerticle)
+                .onSuccess(s -> {
+                    logger.info("Successfully deploying verticle");
+                })
+                .onFailure(e -> {
+                    logger.error("Failed to deploy verticle" , e);
+                });
     }
 }
---
+++ src/test/java/github/clone_code_detection/server/HttpServerVerticleTest.java
@@ -0,0 +1,51 @@
+package github.clone_code_detection.server;
+
+import io.vertx.core.DeploymentOptions;
+import io.vertx.core.Vertx;
+import io.vertx.core.http.HttpClient;
+import io.vertx.core.http.HttpClientResponse;
+import io.vertx.core.http.HttpMethod;
+import io.vertx.core.json.JsonObject;
+import io.vertx.junit5.Checkpoint;
+import io.vertx.junit5.VertxExtension;
+import io.vertx.junit5.VertxTestContext;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+//https://vertx.io/docs/vertx-junit5/java/
+@ExtendWith(VertxExtension.class)
+class HttpServerVerticleTest {
+
+    public static final String EXPECTED =
+            "We are two students from Ho Chi Minh " + "city " + "University of " + "Science.";
+
+    @Test
+    void testHttpServer(Vertx vertx , VertxTestContext vertxTestContext) {
+        HttpServerVerticle httpServerVerticle = new HttpServerVerticle();
+        DeploymentOptions options = new DeploymentOptions().setConfig(new JsonObject());
+        Checkpoint serverSucceeded = vertxTestContext.checkpoint();
+        Checkpoint allSucceeded = vertxTestContext.checkpoint();
+        Checkpoint infoSucceeded = vertxTestContext.checkpoint();
+        vertx.deployVerticle(httpServerVerticle , options , id -> {
+            serverSucceeded.flag();
+
+            HttpClient client = vertx.createHttpClient();
+            client.request(HttpMethod.GET , 8080 , "localhost" , "/api/info/all")
+                    .compose(req -> req.send()
+                            .compose(HttpClientResponse::body))
+                    .onSuccess(buffer -> vertxTestContext.verify(() -> {
+                        assertThat(buffer.toString()).isEqualTo(EXPECTED);
+                        allSucceeded.flag();
+                    }));
+            client.request(HttpMethod.GET , 8080 , "localhost" , "/api/info/authors/1")
+                    .compose(req -> req.send()
+                            .compose(HttpClientResponse::body))
+                    .onSuccess(buffer -> vertxTestContext.verify(() -> {
+                        assertThat(buffer.toString()).isEqualTo("Ha Hai Nguyen");
+                        infoSucceeded.flag();
+                    }));
+        });
+    }
+}
