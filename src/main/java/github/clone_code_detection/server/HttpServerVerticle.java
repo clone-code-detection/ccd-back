e80bcf49cd5b12645d37e10aac00771cb46d6322
hanh4 <hanh4@vng.com.vn>
2023-02-20 00:40:03 +0700

    Inject router

--- src/main/java/github/clone_code_detection/server/HttpServerVerticle.java
+++ src/main/java/github/clone_code_detection/server/HttpServerVerticle.java
@@ -1,7 +1,6 @@
 package github.clone_code_detection.server;
 
-import github.clone_code_detection.routers.AuthorInfoRouter;
-import github.clone_code_detection.routers.CSTProcessorRouter;
+import com.google.inject.Inject;
 import io.netty.handler.codec.http.HttpResponseStatus;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Promise;
@@ -11,13 +10,22 @@ import io.vertx.ext.web.RoutingContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.inject.Named;
+
 public class HttpServerVerticle extends AbstractVerticle {
     private final Logger logger = LoggerFactory.getLogger(HttpServerVerticle.class);
+    private final Router authorInfoRouter;
+    private final Router cstProcessRouter;
+
+    @Inject
+    public HttpServerVerticle(@Named("author") Router authorInfoRouter ,
+                              @Named("cst") Router cstProcessRouter) {
+        this.authorInfoRouter = authorInfoRouter;
+        this.cstProcessRouter = cstProcessRouter;
+    }
 
     private Router createServerRouter() {
         Router router = Router.router(this.vertx);
-        Router authorInfoRouter = new AuthorInfoRouter(this.vertx);
-        Router cstProcessRouter = new CSTProcessorRouter(this.vertx);
         router.get("/api/info/*")
                 .subRouter(authorInfoRouter);
         router.get("/api/cst/*")
