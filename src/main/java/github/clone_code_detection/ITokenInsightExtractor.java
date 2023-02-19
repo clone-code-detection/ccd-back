9624affa4528a140fdab79a96efa1754154e05e3
hanh4 <hanh4@vng.com.vn>
2023-02-20 00:39:41 +0700

    Remove listener and visitor

--- src/main/java/github/clone_code_detection/ITokenInsightExtractor.java
+++
@@ -1,7 +0,0 @@
-package github.clone_code_detection;
-
-import java.util.Collection;
-
-public interface ITokenInsightExtractor {
-    Collection<ExtendListener.TokenInsight> getTokenInsights(String classContent);
-}
