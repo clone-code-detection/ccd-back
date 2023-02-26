0febbee36f23f8aa3603fd7dfe62e8203471a411
Nguyễn Hải Hà <59325144+haiha0201@users.noreply.github.com>
2023-02-26 12:17:00 +0700

    Feature/ccd 36 (#11)
    * Remove .g4 files
    
    * Migrate to spring
    
    ---------
    
    Co-authored-by: hanh4 <hanh4@vng.com.vn>

--- src/main/java/github/clone_code_detection/App.java
+++ src/main/java/github/clone_code_detection/App.java
@@ -1,43 +1,12 @@
 package github.clone_code_detection;
 
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.google.inject.Guice;
-import com.google.inject.Injector;
-import github.clone_code_detection.server.HttpServerVerticle;
-import io.vertx.core.Verticle;
-import io.vertx.core.Vertx;
-import io.vertx.core.json.jackson.DatabindCodec;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/**
- * Hello world!
- */
+@SpringBootApplication(scanBasePackages = {"github.clone_code_detection"})
 public class App {
-    static private final Logger logger = LoggerFactory.getLogger(App.class);
-    static Module module = new Module();
-    static Injector injector = Guice.createInjector(module);
-
     public static void main(String[] args) {
-        setUp();
-        deployOnAllCore(HttpServerVerticle.class);
-    }
-
-    private static void setUp() {
-        DatabindCodec.mapper().configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-    }
-
-    private static void deployOnAllCore(Class<? extends Verticle> clazz) {
-        Vertx vertx = injector.getInstance(Vertx.class);
-        for (int i = 0; i < Runtime.getRuntime().availableProcessors(); i++) {
-            Verticle verticle = injector.getInstance(clazz);
-            vertx.deployVerticle(verticle)
-                    .onSuccess(s -> {
-                        logger.info("Successfully deploying verticle");
-                    })
-                    .onFailure(e -> {
-                        logger.error("Failed to deploy verticle", e);
-                    });
-        }
+        SpringApplication.run(App.class , args);
     }
 }
+
