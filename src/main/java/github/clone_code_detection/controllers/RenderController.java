253a820ef6e116178634dec5401cad1aa020043c
zenodinh <quandinh33@gmail.com>
2023-03-21 09:45:29 +0700

    [CCD-47] Add ServiceRender

--- src/main/java/github/clone_code_detection/controllers/RenderController.java
+++ src/main/java/github/clone_code_detection/controllers/RenderController.java
@@ -1,6 +1,5 @@
 package github.clone_code_detection.controllers;
 
-import co.elastic.clients.util.Pair;
 import github.clone_code_detection.entity.RenderDocument;
 import github.clone_code_detection.entity.ResponseUnified;
 import github.clone_code_detection.service.ServiceRender;
@@ -8,6 +7,9 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.sql.SQLException;
+import java.util.MissingResourceException;
 
 @Slf4j
 @RestController
@@ -24,14 +26,18 @@ public class RenderController {
     @RequestMapping(path = "/{id}", method = RequestMethod.GET)
     public ResponseUnified<RenderDocument> getRenderDocument(@PathVariable("id") int id) {
         log.info("Render report id: {}", id);
-        Pair<RenderDocument, Integer> response = serviceRender.getRenderDocument(id);
-        if (response == null)
-            return new ResponseUnified<>("Can not get render data", HttpServletResponse.SC_INTERNAL_SERVER_ERROR, null);
-        return switch (response.value()) {
-            case 0 -> new ResponseUnified<>("Success", HttpServletResponse.SC_OK, response.key());
-            case 1 -> new ResponseUnified<>("Report not found", HttpServletResponse.SC_BAD_REQUEST, null);
-            case 2 -> new ResponseUnified<>("Load report failed", HttpServletResponse.SC_INTERNAL_SERVER_ERROR, null);
-            default -> new ResponseUnified<>("unspecified", HttpServletResponse.SC_EXPECTATION_FAILED, null);
-        };
+        RenderDocument response;
+        try {
+            response = serviceRender.getRenderDocument(id);
+            if (response == null)
+                throw new NullPointerException("Can not get render data");
+            return new ResponseUnified<>("Success", HttpServletResponse.SC_OK, response);
+        } catch (Exception e) {
+            if (e.getClass() == SQLException.class || e.getClass() == NegativeArraySizeException.class)
+                new ResponseUnified<>(e.getMessage(), HttpServletResponse.SC_BAD_REQUEST, null);
+            if (e.getClass() == MissingResourceException.class || e.getClass() == NullPointerException.class)
+                return new ResponseUnified<>(e.getMessage(), HttpServletResponse.SC_INTERNAL_SERVER_ERROR, null);
+            return new ResponseUnified<>("Success", HttpServletResponse.SC_SERVICE_UNAVAILABLE, null);
+        }
     }
 }
