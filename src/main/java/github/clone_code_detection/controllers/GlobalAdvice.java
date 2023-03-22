374c5ff4e538e685b0523ee67c6e4d35a71d4df0
haiha <thiensuha01@gmail.com>
2023-03-22 08:23:02 +0700

    Catch all errors

--- src/main/java/github/clone_code_detection/controllers/GlobalAdvice.java
+++ src/main/java/github/clone_code_detection/controllers/GlobalAdvice.java
@@ -3,36 +3,49 @@ package github.clone_code_detection.controllers;
 import github.clone_code_detection.entity.ResponseUnified;
 import jakarta.validation.ConstraintViolationException;
 import jakarta.validation.ValidationException;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.validation.BindException;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.text.MessageFormat;
+
 
 @RestControllerAdvice
-@ControllerAdvice
-class GlobalAdvice extends ResponseEntityExceptionHandler {
+class GlobalAdvice {
     @ExceptionHandler(value = {AuthenticationException.class})
-    public ResponseEntity<Object> handleAuthentication(RuntimeException ex, WebRequest request) {
-        ResponseUnified<String> response = ResponseUnified.<String>builder()
-                                                          .code(-1)
-                                                          .data(ex.getMessage())
-                                                          .build();
-        return handleExceptionInternal(ex, response, new HttpHeaders(), HttpStatus.UNAUTHORIZED, request);
+    public ResponseUnified<String> handleAuthentication(RuntimeException ex) {
+        return ResponseUnified.<String>builder()
+                              .code(-1)
+                              .data(ex.getMessage())
+                              .build();
     }
 
     @ExceptionHandler(value = {ConstraintViolationException.class, ValidationException.class, MethodArgumentNotValidException.class})
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ResponseUnified<String> handleConstraints(RuntimeException ex) {
+    public ResponseUnified<String> handleConstraints(Exception ex) {
         return ResponseUnified.<String>builder()
                               .code(-2)
                               .data(ex.getMessage())
+                              .build();
+    }
+
+    @ExceptionHandler(value = {BindException.class})
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseUnified<String> handleConstraints(BindException ex) {
+        ObjectError message = ex.getAllErrors()
+                                .get(0);
+
+        return ResponseUnified.<String>builder()
+                              .code(-2)
+                              .data(MessageFormat.format("{0} : {1}",
+                                                         message.getObjectName(),
+                                                         message.getDefaultMessage()))
                               .build();
     }
 
