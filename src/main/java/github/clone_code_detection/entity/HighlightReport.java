59112d07d2928fa497b23a90b0e0655cb89cacec
zenodinh <quandinh33@gmail.com>
2023-03-25 11:11:38 +0700

    Fix database conflict

--- src/main/java/github/clone_code_detection/entity/HighlightReport.java
+++ src/main/java/github/clone_code_detection/entity/HighlightReport.java
@@ -8,7 +8,9 @@ import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
+import java.time.ZoneId;
 import java.time.ZonedDateTime;
+import java.util.HashSet;
 import java.util.Set;
 
 @Entity(name = "highlight_report")
@@ -21,30 +23,38 @@ public class HighlightReport {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     int id;
-    @Column(columnDefinition = "nvarchar(255) default ''")
-    String organization;
-    @Column(columnDefinition = "nvarchar(255) default ''")
-    String course;
-    @Column(columnDefinition = "int not null default 0")
-    int year;
-    @Column(columnDefinition = "int not null default 1")
-    int semester;
-    @Column(columnDefinition = "nvarchar(255) default ''")
-    String author;
-    @Column(columnDefinition = "nvarchar(255) default ''")
-    String project;
-    @Column(columnDefinition = "nvarchar(255) default ''")
-    String uri;
+    @Column(nullable = false)
+    @Builder.Default
+    String organization = "test";
+    @Column(nullable = false)
+    @Builder.Default
+    String course = "test";
+    @Column(nullable = false)
+    @Builder.Default
+    int year = ZonedDateTime.now(ZoneId.of("Asia/Ho_Chi_Minh")).getYear();
+    @Column(nullable = false)
+    @Builder.Default
+    int semester = 1;
+    @Column(nullable = false)
+    @Builder.Default
+    String author = "participant";
+    @Column(nullable = false)
+    @Builder.Default
+    String project = "test";
+    @Column(nullable = false)
+    String uri = "";
     @ElementCollection
     @CollectionTable(name = "highlight_report_extra_data")
-    @Column(name = "extra_data", columnDefinition = "text not null")
-    Set<String> extraData;
+    @Column(name = "extra_data")
+    @Builder.Default
+    Set<String> extraData = new HashSet<>();
     @CreationTimestamp
-    @Column(name = "created_at", columnDefinition = "datetime not null default now()", nullable = false)
+    @Column(name = "created_at", nullable = false)
     ZonedDateTime createdAt;
     @UpdateTimestamp
-    @Column(name = "updated_at", columnDefinition = "datetime not null default now() on update now()", nullable = false)
+    @Column(name = "updated_at", nullable = false)
     ZonedDateTime updatedAt;
     @Column(name = "assigner", nullable = false)
-    String assigner;
+    @Builder.Default
+    String assigner = "admin";
 }
