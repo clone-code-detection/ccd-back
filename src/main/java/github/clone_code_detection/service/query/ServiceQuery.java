202985920bd8a771c27807ea125b2a384c3ced9e
haiha <thiensuha01@gmail.com>
2023-05-28 21:19:36 +0700

    [Feature CCD-92] Add single-source-match overview displaying author, score and match_id of matched files

--- src/main/java/github/clone_code_detection/service/query/ServiceQuery.java
+++ src/main/java/github/clone_code_detection/service/query/ServiceQuery.java
@@ -1,11 +1,17 @@
 package github.clone_code_detection.service.query;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import github.clone_code_detection.entity.ElasticsearchDocument;
+import github.clone_code_detection.entity.authenication.UserImpl;
+import github.clone_code_detection.entity.highlight.document.HighlightSingleDocument;
+import github.clone_code_detection.entity.highlight.document.HighlightSingleTargetMatchDocument;
 import github.clone_code_detection.entity.query.QueryInstruction;
 import github.clone_code_detection.exceptions.es.ElasticsearchResponseParseException;
 import github.clone_code_detection.repo.RepoElasticsearchQuery;
+import github.clone_code_detection.repo.RepoHighlightSingleMatchDocument;
 import github.clone_code_detection.util.JsonUtil;
+import lombok.Builder;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.elasticsearch.search.SearchHit;
@@ -14,17 +20,21 @@ import org.springframework.stereotype.Service;
 
 import javax.annotation.Nonnull;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
+import java.util.UUID;
 import java.util.stream.Collectors;
 
 @Service
 @Slf4j
 public class ServiceQuery implements IServiceQuery {
     private final RepoElasticsearchQuery repoElasticsearchQuery;
+    private final RepoHighlightSingleMatchDocument repoHighlightSingleMatchDocument;
 
     @Autowired
-    public ServiceQuery(RepoElasticsearchQuery repoElasticsearchQuery) {
+    public ServiceQuery(RepoElasticsearchQuery repoElasticsearchQuery, RepoHighlightSingleMatchDocument repoHighlightSessionDocument) {
         this.repoElasticsearchQuery = repoElasticsearchQuery;
+        this.repoHighlightSingleMatchDocument = repoHighlightSessionDocument;
     }
 
     @SneakyThrows
@@ -44,5 +54,48 @@ public class ServiceQuery implements IServiceQuery {
         } catch (JsonProcessingException e) {
             throw new ElasticsearchResponseParseException("Failed to parse elasticsearch response", e);
         }
+    }
+
+    public Collection<TargetMatchOverview> handle(String id) {
+        UUID fromString;
+        try {
+            fromString = UUID.fromString(id);
+        } catch (Exception ig) {
+            throw new RuntimeException("Invalid id", ig);
+        }
+        HighlightSingleDocument singleDocument = repoHighlightSingleMatchDocument.findById(fromString)
+                                                                                 .orElseThrow();
+        return singleDocument.getMatches()
+                             .stream()
+                             .map(ServiceQuery::extract)
+                             .toList();
+    }
+
+    private static TargetMatchOverview extract(HighlightSingleTargetMatchDocument document) {
+        UserImpl user = document.getTarget()
+                                .getUser();
+        String userName = user != null ? user.getUsername() : "anonymous";
+        String targetId = document.getId()
+                                  .toString();
+        Float score = document.getScore();
+        return TargetMatchOverview.builder()
+                                  .author(userName)
+                                  .id(targetId)
+                                  .score(score)
+                                  .build();
+    }
+
+    @Builder
+    public static class TargetMatchOverview {
+        @JsonProperty("author")
+        private String author;
+
+        @JsonProperty("score")
+        private double score;
+
+        @JsonProperty("target_match_id")
+        private String id;
+
+        // TODO: created time, origin
     }
 }
