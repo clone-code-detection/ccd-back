3c249e5f8c2ebd5ae51c46f3ac90395bbbe77a7f
haiha <thiensuha01@gmail.com>
2023-06-10 12:39:13 +0700

    [Bug] Fix user reference persistence not working

---
+++ src/main/java/github/clone_code_detection/repo/RepoUserReference.java
@@ -0,0 +1,11 @@
+package github.clone_code_detection.repo;
+
+import github.clone_code_detection.entity.moodle.UserReference;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface RepoUserReference extends JpaRepository<UserReference, UUID> {
+}
